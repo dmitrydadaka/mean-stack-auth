31f2f2bb7d7977fd05d52470fef241f2ee52808e
Dmitry Dadaka <belpc2012@gmail.com>
2024-12-29 18:05:37 +0300

    feat: forget password client and model for backend

--- client/src/app/pages/forget-password/forget-password.component.ts
+++ client/src/app/pages/forget-password/forget-password.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-forget-password',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   templateUrl: './forget-password.component.html',
   styleUrl: './forget-password.component.scss'
 })
-export default class ForgetPasswordComponent {
+
+export default class ForgetPasswordComponent implements OnInit {
+  forgetForm !: FormGroup;
+  fb = inject(FormBuilder);
+
+  ngOnInit(): void {
+    this.forgetForm = this.fb.group({
+      email: ["", Validators.compose([Validators.required, Validators.email])],
+    }
+    );
+  }
+
+  submit() {
+    console.log(this.forgetForm.value);
+  }
 
 }
